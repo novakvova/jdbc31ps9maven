08449e06783b563442eaa1366c5bee318e35db88
novakvova <novakvova@gmail.com>
2020-02-01 11:23:35 +0200

    Init commit

---
+++ JDBCMaven/src/main/java/sql/postgres.sql
@@ -0,0 +1,55 @@
+psql -U postgres
+
+CREATE USER vova with PASSWORD 'Qwerty1-';
+
+\du
+
+CREATE DATABASE paraskadb;
+
+\l
+
+GRANT ALL PRIVILEGES ON DATABASE "paraskadb" to vova;
+
+\q
+
+
+CREATE TABLE tbl_employees
+(
+    ID SERIAL PRIMARY KEY,
+    NAME VARCHAR(100) NOT NULL,
+    SALARY decimal(15, 2) NOT NULL,
+    CREATED_DATE timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-------------------------NEW UPDATE---------------------------------------
+DROP TABLE IF EXISTS books, authors, testing, images;
+
+CREATE TABLE IF NOT EXISTS authors (
+    id serial PRIMARY KEY, 
+    name VARCHAR(25)
+);
+
+CREATE TABLE IF NOT EXISTS books (
+    id serial PRIMARY KEY, 
+    author_id INT references authors(id), title VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS testing(id INT);
+CREATE TABLE IF NOT EXISTS images(id serial, data bytea);
+
+INSERT INTO authors(id, name) VALUES(1, 'Jack London');
+INSERT INTO authors(id, name) VALUES(2, 'Honore de Balzac');
+INSERT INTO authors(id, name) VALUES(3, 'Lion Feuchtwanger');
+INSERT INTO authors(id, name) VALUES(4, 'Emile Zola');
+INSERT INTO authors(id, name) VALUES(5, 'Truman Capote');
+
+INSERT INTO books(id, author_id, title) VALUES(1, 1, 'Call of the Wild');
+INSERT INTO books(id, author_id, title) VALUES(2, 1, 'Martin Eden');
+INSERT INTO books(id, author_id, title) VALUES(3, 2, 'Old Goriot');
+INSERT INTO books(id, author_id, title) VALUES(4, 2, 'Cousin Bette');
+INSERT INTO books(id, author_id, title) VALUES(5, 3, 'Jew Suess');
+INSERT INTO books(id, author_id, title) VALUES(6, 4, 'Nana');
+INSERT INTO books(id, author_id, title) VALUES(7, 4, 'The Belly of Paris');
+INSERT INTO books(id, author_id, title) VALUES(8, 5, 'In Cold blood');
+INSERT INTO books(id, author_id, title) VALUES(9, 5, 'Breakfast at Tiffany');
